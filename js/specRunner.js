885d37be2ff06045a559b0f0d48b008786a6a241
John Rocha <jacolady@gmail.com>
2015-02-22 19:46:11 +0000

    Add Jasmine tests (patched for compatibility with RequiresJS)

---
+++ js/specRunner.js
@@ -0,0 +1,46 @@
+(function() {
+    'use strict';
+
+    // Helper for compatibility with RequireJS
+    // Based on http://stackoverflow.com/questions/19240302/does-jasmine-2-0-really-not-work-with-require-js
+
+    // Configure RequireJS to shim Jasmine
+    require.config({
+        baseUrl: 'js',
+        paths: {
+            'jasmine': '../lib/jasmine-2.1.3/jasmine',
+            'jasmine-html': '../lib/jasmine-2.1.3/jasmine-html',
+            'boot': '../lib/jasmine-2.1.3/boot'
+        },
+        shim: {
+            'jasmine': {
+                exports: 'window.jasmineRequire'
+            },
+            'jasmine-html': {
+                deps: ['jasmine'],
+                exports: 'window.jasmineRequire'
+            },
+            'boot': {
+                deps: ['jasmine', 'jasmine-html'],
+                exports: 'window.jasmineRequire'
+            }
+        }
+    });
+
+    // Define all of your specs here. These are RequireJS modules.
+    var specs = [
+        '../spec/parser_spec'
+    ];
+
+    // Load Jasmine - This will still create all of the normal Jasmine browser globals unless `boot.js` is re-written to use the
+    // AMD or UMD specs. `boot.js` will do a bunch of configuration and attach it's initializers to `window.onload()`. Because
+    // we are using RequireJS `window.onload()` has already been triggered so we have to manually call it again. This will
+    // initialize the HTML Reporter and execute the environment.
+    require(['boot'], function () {
+        // Load the specs
+        require(specs, function () {
+            // Initialize the HTML Reporter and execute the environment (setup by `boot.js`)
+            window.onload();
+        });
+    });
+})();
---
+++ spec/parser_spec.js
@@ -0,0 +1,46 @@
+define(['parser'], function(CurrencyParser) {
+    'use strict';
+
+    describe('CurrencyParser', function() {
+        var parser;
+
+        beforeEach(function() {
+            parser = new CurrencyParser();
+        });
+
+        xit('can\'t parse numbers', function() {
+            expect(function() {
+                parser.parseString(10);
+            }).toThrow();
+        });
+
+        it('can correctly parse valid string formats', function() {
+            // Based on table in gist
+            // TODO DRY using where.js - and get better descriptions for free!
+            // TODO or at least do this with an array :(
+            expect(parser.parseString('4')).toEqual(4);
+            expect(parser.parseString('85')).toEqual(85);
+            expect(parser.parseString('197p')).toEqual(197);
+            expect(parser.parseString('2p')).toEqual(2);
+            expect(parser.parseString('1.87')).toEqual(187);
+            expect(parser.parseString('£1.23')).toEqual(123);
+            expect(parser.parseString('£2')).toEqual(200);
+            expect(parser.parseString('£10')).toEqual(1000);
+            expect(parser.parseString('£1.87p')).toEqual(187);
+            expect(parser.parseString('£1p')).toEqual(100);
+            expect(parser.parseString('£1.p')).toEqual(100);
+            expect(parser.parseString('001.41p')).toEqual(141);
+            expect(parser.parseString('4.235p')).toEqual(424);
+            expect(parser.parseString('£1.257422457p')).toEqual(126);
+        });
+
+        it('returns 0 for invalid string formats', function() {
+            // As above
+            // TODO assumption is that no error is thrown, we just get zero back
+            expect(parser.parseString('')).toEqual(0);
+            expect(parser.parseString('1x')).toEqual(0);
+            expect(parser.parseString('£1x.0p')).toEqual(0);
+            expect(parser.parseString('£p')).toEqual(0);
+        });
+    });
+});
