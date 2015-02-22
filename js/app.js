275bca46427b24233d11df119aee2e771caca87e
John Rocha <jacolady@gmail.com>
2015-02-22 14:06:02 +0000

    Handle non-JavaScript clients; add coins automatically

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// Enable form, since JavaScript is working
+var form = document.getElementById('genieForm');
+for (var i = 0; i < form.elements.length; i++)
+  form.elements[i].disabled = false;
+
+document.getElementById('genieForm').addEventListener('submit', function (evt) {
+  var coinsDiv = document.getElementById('coins');
+  var coins = ['£2', '£2', '50p', '20p', '5p'];
+  for (var i = 0; i < coins.length; i++) {
+    var item = document.createElement('li');
+    item.appendChild(document.createTextNode(coins[i]));
+    coinsDiv.appendChild(item);
+  }
+  evt.preventDefault();
+});
+
