8649b30747907f66d9ac091c180a45631a698125
John Rocha <jacolady@gmail.com>
2015-02-22 16:09:26 +0000

    Use domReady plugin

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-define(['cashier'], function (Cashier) {
+define(['cashier', 'domReady!'], function (Cashier, doc) {
+    'use strict';
+
     // Enable form, since JavaScript is working
-    var form = document.getElementById('genieForm');
+    var form = doc.getElementById('genieForm');
     for (var i = 0; i < form.elements.length; i++)
         form.elements[i].disabled = false;
 
-    cashier = new Cashier();
+    var cashier = new Cashier();
     console.log(cashier.currency);
 
-    document.getElementById('genieForm').addEventListener('submit', function (evt) {
-        var coinsDiv = document.getElementById('coins');
+    doc.getElementById('genieForm').addEventListener('submit', function (evt) {
+        var coinsDiv = doc.getElementById('coins');
         var coins = ['£2', '£2', '50p', '20p', '5p'];
         for (var i = 0; i < coins.length; i++) {
-            var item = document.createElement('li');
-            item.appendChild(document.createTextNode(coins[i]));
+            var item = doc.createElement('li');
+            item.appendChild(doc.createTextNode(coins[i]));
             coinsDiv.appendChild(item);
         }
         evt.preventDefault();
