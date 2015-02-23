bb6fd82ceb59d6b6cfad2428cf72cb7668eb8322
John Rocha <jacolady@gmail.com>
2015-02-23 11:38:42 +0000

    Allow custom parser formats

--- js/app/Parser.js
+++ js/app/Parser.js
@@ -2,31 +2,30 @@ define(function() {
     'use strict';
 
     /**
+     * Creates a new CurrencyParser
      * @class
      */
-    function CurrencyParser() { }
-
-    /**
-     * Array of recognised currency formats, each with a regex pattern
-     * and a parser function which receives the first matched group.
-     */
-    var formats = [
-        {
-            // Just pence, with optional 'p'
-            pattern: /^(\d+)p?$/,
-            parser: function (str) { return parseInt(str); }
-        },
-        {
-            // Just pounds, with '£' and optional 'p'
-            pattern: /^£(\d+)p?$/,
-            parser: function (str) { return parseInt(str) * 100; }
-        },
-        {
-            // Pounds and pence, with decimal point, and optional '£' and/or 'p'
-            pattern: /^£?(\d+(\.\d*))p?$/,
-            parser: function (str) { return Math.round(parseFloat(str) * 100); }
-        },
-    ];
+    function CurrencyParser() {
+        // Default currency formats, each with a regex pattern and a parser
+        // function that receives the first matched regex group.
+        this._formats = [
+            {
+                // Just pence, with optional 'p'
+                pattern: /^(\d+)p?$/,
+                parser: function (str) { return parseInt(str); }
+            },
+            {
+                // Just pounds, with '£' and optional 'p'
+                pattern: /^£(\d+)p?$/,
+                parser: function (str) { return parseInt(str) * 100; }
+            },
+            {
+                // Pounds and pence, with decimal point, and optional '£' and/or 'p'
+                pattern: /^£?(\d+(\.\d*))p?$/,
+                parser: function (str) { return Math.round(parseFloat(str) * 100); }
+            },
+        ];
+    }
 
     /**
      * Parse the specified string and return a number of pence Sterling
@@ -34,8 +33,8 @@ define(function() {
      * @returns {Number} Value in pence, or undefined if parsing failed
      */
     CurrencyParser.prototype.parseString = function(input) {
-        for (var i = 0; i < formats.length; i++) {
-            var format = formats[i];
+        for (var i = 0; i < this._formats.length; i++) {
+            var format = this._formats[i];
             var matches = input.match(format.pattern);
             if (matches)
                 return format.parser(matches[1]);
@@ -43,6 +42,21 @@ define(function() {
 
         // Return undefined if no format was matched
     };
+
+    /**
+     * Add a custom currency format to be tried after the defaults
+     * @param {RegExp} newPattern - Regular expression for the currency format
+     * @param {CurrencyParser~parserCallback} newParser - Function to parse this format
+     */
+    CurrencyParser.prototype.addFormat = function(newPattern, newParser) {
+        this._formats.push({ pattern: newPattern, parser: newParser });
+    }
+
+    /**
+     * Callback to parse a currency format
+     * @callback CurrencyParser~parserCallback
+     * @param {string} str - The value of the first matching regex group
+     */
 
     // Return constructor
     return CurrencyParser;
--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ define(['app/Cashier', 'app/Parser', 'app/View', 'lib/domReady!'], function (Cas
     var parser = new CurrencyParser();
     var view = new View(doc);
 
+    // Example of adding custom currency formats to the parser */
+    //parser.addFormat(/^(\d+) pence$/, function (str) { return parseInt(str); });
+
     view.onInput(function (amountStr) {
         var amountInPence = parser.parseString(amountStr);
         //TODO handle strings that could not be parsed
