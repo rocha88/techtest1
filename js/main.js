71302d331290e13e2272836f1507c69697c58e1c
John Rocha <jacolady@gmail.com>
2015-02-23 12:07:25 +0000

    Clean up formatting

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-define(['app/Cashier', 'app/Parser', 'app/View', 'lib/domReady!'], function (Cashier, CurrencyParser, View, doc) {
-    'use strict';
+define(
+    ['app/Cashier', 'app/Parser', 'app/View', 'lib/domReady!'], 
+    function (Cashier, CurrencyParser, View, doc) {
+        'use strict';
 
-    var cashier = new Cashier();
-    var parser = new CurrencyParser();
-    var view = new View(doc);
+        var cashier = new Cashier();
+        var parser = new CurrencyParser();
+        var view = new View(doc);
 
-    // Example of adding custom currency formats to the parser */
-    //parser.addFormat(/^(\d+) pence$/, function (str) { return parseInt(str); });
+        // Example of adding custom currency formats to the parser */
+        //parser.addFormat(/^(\d+) pence$/, function (str) { return parseInt(str); });
 
-    view.onInput(function (amountStr) {
-        var amountInPence = parser.parseString(amountStr);
-        //TODO handle strings that could not be parsed
-        var coins = cashier.coinsFor(amountInPence);
-        view.setOutput(coins);
-    });
-});
+        view.onInput(function (amountStr) {
+            var amountInPence = parser.parseString(amountStr);
+            //TODO handle strings that could not be parsed
+            var coins = cashier.coinsFor(amountInPence);
+            view.setOutput(coins);
+        });
+    }
+);
